130f57baf4f1f127b3e243a6af3b2378c79550af
Meilashinta Putri Yuliantoni <meilashinta26@gmail.com>
2025-06-27 22:05:47 +0700

    fix: change all page titles to Indonesian language(FrontEnd)

--- src/resources/views/admin/gallery/index.blade.php
+++ src/resources/views/admin/gallery/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <div class="container-fluid">
         <div class="d-flex justify-content-between align-items-center mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Manage Gallery</h1>
+            <h1 class="h3 mb-0 text-gray-800">Kelola Galeri</h1>
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createGalleryModal">
-                <i class="fas fa-plus"></i> Add New Gallery
+                <i class="fas fa-plus"></i> Tambah Galeri Baru
             </button>
         </div>
 
@@ -25,7 +25,7 @@
 
         <div class="card shadow mb-4">
             <div class="card-header py-3 d-flex justify-content-between align-items-center">
-                <h6 class="m-0 font-weight-bold text-primary">Gallery List</h6>
+                <h6 class="m-0 font-weight-bold text-primary">Daftar Galeri</h6>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
